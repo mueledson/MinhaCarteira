133a069dac4f425d52872240ceac29a0c32022bd
edson@app100.com.br <mueledson>
2023-07-30 17:31:40 -0300

    Fizemos os temas funcionarem só que não está automático

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { ThemeProvider } from "styled-components"
 import GlobalStyles from "./styles/GlobalStyles";
 
 import Layout from "./components/Layout";
+import dark from "./styles/themes/dark";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import light from "./styles/themes/light";
 
 const App: React.FC = () => {
     return (
-            <>
+            <ThemeProvider theme={dark}>
                 <GlobalStyles/>
                 <Layout/>
-            </>
+            </ThemeProvider>
     )
 }
 
