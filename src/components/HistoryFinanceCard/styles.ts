3d7bd5d0dcce8217c19b2ce8f689add4a17912c3
edson@app100.com.br <mueledson>
2023-08-06 11:58:03 -0300

    Nossas listas de Entradas e Saídas estão de acordo os arquivos

--- src/components/HistoryFinanceCard/styles.ts
+++ src/components/HistoryFinanceCard/styles.ts
@@ -9,7 +9,7 @@ export const Container = styled.li`
     background-color: ${props => props.theme.colors.tertiary};
 
     list-style: none;
-    border-radius: 5px;
+    border-radius: 10px;
 
     margin: 10px 0;
     padding: 12px 10px;
@@ -35,11 +35,15 @@ export const Container = styled.li`
         padding-left: 10px;
     }
 
+    > div span{
+        font-weight: 500;
+        font-size: 22px;
+    }
 `;
 
 export const Tag = styled.div<ITagProps>`
-    width: 10px;
-    height: 30px;
+    width: 13px;
+    height: 60%;
 
     background-color: ${props => props.color};
 
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,12 +1,25 @@
-import { useMemo } from "react";
-import { useParams } from "react-router-dom";
+import { useMemo, useState, useEffect } from "react"; import { useParams } from "react-router-dom";
 
 import SelectInput from "../../components/SelectInput";
 import { Container, Content, Filters } from "./styles";
 import ContentHeader from "../../components/ContentHeader";
 import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 
+import gains from "../../repositories/gains";
+import expenses from "../../repositories/expenses";
+
+interface IData {
+    id: string;
+    description: string;
+    amountFormatted: string;
+    frequency: string;
+    dataFormatted: string;
+    tagColor: string;
+}
+
 function List(){
+    const [ data, setData ] = useState<IData[]>([])
+
     const { type } = useParams(); 
     
     const title = useMemo(() => {
@@ -16,6 +29,10 @@ function List(){
     const lineColor = useMemo(() => {
         return type === 'entry-balance' ? '#F7931B' : '#E44C4E'
     }, [type]);
+
+    const listData = useMemo(() =>{
+        return type === 'entry-balance' ? gains : expenses
+    }, [type])
 
     const months = [
         {value: 8, label: 'Agosto'},
@@ -27,6 +44,21 @@ function List(){
         {value: 2024, label: 2024},
         {value: 2025, label: 2025}
     ]
+
+    useEffect(() => {
+       const response = listData.map(item => {
+            return {
+                id: String(Math.random () * data.length),
+                description: item.description,
+                amountFormatted: item.amount,
+                frequency: item.frequency,
+                dataFormatted: item.date,
+                tagColor: item.frequency === 'recorrente' ? '#4E41F0' : '#E44C4E'
+            }
+        })
+
+        setData(response)
+    }, [])
 
     return (
         <Container>
@@ -51,12 +83,17 @@ function List(){
             </Filters>
 
             <Content>
-                <HistoryFinanceCard 
-                    tagColor={lineColor}
-                    title="Conta de Luz"
-                    subtitle="27/05/2021"
-                    amount="R$ 100,00"
-                />
+                {
+                    data.map(item => (
+                        <HistoryFinanceCard 
+                            key={item.id}
+                            tagColor={item.tagColor}
+                            title={item.description}
+                            subtitle={item.dataFormatted}
+                            amount={item.amountFormatted}
+                        />
+                    ))
+                }
             </Content>
         </Container>
     )
